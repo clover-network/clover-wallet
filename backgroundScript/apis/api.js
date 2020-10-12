d067caf43512be8d5f07429eb3137b76a5c46cee
werlandy <422606101@qq.com>
2020-10-12 12:30:37 +0800

    bug fix: load pre-defined types when connecting

--- backgroundScript/apis/api.js
+++ backgroundScript/apis/api.js
@@ -29,21 +29,21 @@ const disconnect = () => {
 
 const connect = async network => {
   let api;
-  const { networkFullUrl, name } = network;
-  if (name === 'dotcustom') {
+  const { networkFullUrl, value } = network;
+  if (value === 'dotcustom') {
     disconnect();
   }
   if (networkFullUrl !== undefined && networkFullUrl !== null && networkFullUrl !== '') {
     disconnect();
     try {
       const provider = new WsProvider(networkFullUrl);
-      if (name === EDGEWARE_NETWORK.name || name === BERESHEET_NETWORK.name) {
+      if (value === EDGEWARE_NETWORK.value || value === BERESHEET_NETWORK.value) {
         api = await ApiPromise.create({
           provider,
           types: edgeWareTypes,
           typesAlias,
         });
-      } else if (name === CLOVER_NETWORK.name) {
+      } else if (value === CLOVER_NETWORK.value) {
         api = await ApiPromise.create({
           provider,
           types: cloverTypes
