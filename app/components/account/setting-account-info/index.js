3590766abe75e103f5511bfb5fc89d863c05a0af
Taneeya <2240351540@qq.com>
2020-11-05 13:41:23 +0800

    modify setting page

---
+++ app/components/account/setting-account-info/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './styles.css';
+import { shortenAddress } from '../../../services/wallet-service';
+
+export default class SettingAccountDetails extends Component {
+  render() {
+    const { alias, address, ...otherProps } = this.props;
+    return (
+      <div className="account-info-wrapper" {...otherProps}>
+        <span className="account-info-alias">{alias}</span>
+        <span className="account-info-address">{shortenAddress(address)}</span>
+        <div className="account-connect-status">
+          <span>Connected</span>
+        </div>
+      </div>
+    );
+  }
+}
--- app/views/settings/settings.component.js
+++ app/views/settings/settings.component.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './styles.css';
+import Button from '@material-ui/core/Button';
 import Close from '../../images/close.svg';
-import { shortenAddress } from '../../services/wallet-service';
+import SettingAccountDetails from '../../components/account/setting-account-info';
+import ManageAccount from '../../images/manage_account.svg';
+import ArrowRight from '../../images/arrow_right.svg';
 
 export default class Settings extends Component {
   handleClose = () => {
@@ -10,13 +13,61 @@ export default class Settings extends Component {
 
   render() {
     const { account } = this.props;
+    const settingLists = [
+      {
+        name: 'Address Book',
+      },
+      {
+        name: 'Terms Of Use',
+      },
+      {
+        name: 'About Clover Wallet Extension',
+      },
+    ];
     return (
       <div className="container">
-        <span>{account.alias}</span>
-        <span>{shortenAddress(account.address)}</span>
+        <div style={{ height: '470px', borderBottom: '1px solid rgba(0, 0, 0, 0.05)' }}>
+          <SettingAccountDetails alias={account.alias} address={account.address} />
+          <Button variant="contained" className="manage-account-btn">
+            <img
+              width="22"
+              height="22"
+              src={ManageAccount}
+              aria-hidden="true"
+              alt="staking"
+              style={{ marginRight: '10px' }}
+            />
+            MANAGE ACCOUNTS
+          </Button>
+          <div className="setting-list-wrap">
+            {settingLists.map((setting, idx) => (
+              <SettingList settingName={setting.name} key={`setting_${idx.toString()}}`} />
+            ))}
+          </div>
+        </div>
         <div className="footer" onClick={this.handleClose}>
           <img src={Close} alt="close" aria-hidden="true" width="20" />
           <span className="close">CLOSE</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class SettingList extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="setting-list-item">
+        <div className="setting-list-left">
+          <span>{this.props.settingName}</span>
+        </div>
+        <div className="setting-list-right">
+          <img src={ArrowRight} alt="" />
         </div>
       </div>
     );
