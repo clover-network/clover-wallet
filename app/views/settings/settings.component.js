0887ab307071b94f9864cb42a33c48bf7438cc6b
werlandy <422606101@qq.com>
2020-11-05 09:59:51 +0800

    add settings info

--- app/views/settings/settings.component.js
+++ app/views/settings/settings.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Close from '../../images/close.svg';
+import { shortenAddress } from '../../services/wallet-service';
 
 export default class Settings extends Component {
   handleClose = () => {
@@ -8,8 +9,11 @@ export default class Settings extends Component {
   };
 
   render() {
+    const { account } = this.props;
     return (
       <div className="container">
+        <span>{account.alias}</span>
+        <span>{shortenAddress(account.address)}</span>
         <div className="footer" onClick={this.handleClose}>
           <img src={Close} alt="close" aria-hidden="true" width="20" />
           <span className="close">CLOSE</span>
