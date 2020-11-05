21c108a01bae43ff45aeca8437c1997e3be800d3
Taneeya <2240351540@qq.com>
2020-11-05 09:52:03 +0800

    modify account detail page

---
+++ app/components/header-back/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ArrowLeft from '../../images/arrow_left.svg';
+import './styles.css';
+
+export default class HeaderBack extends Component {
+  render() {
+    const { handleBack, title, ...otherProps } = this.props;
+    return (
+      <div {...otherProps} className="header-wrapper">
+        <div className="header-back-btn" onClick={handleBack}>
+          <img width="12" height="12" src={ArrowLeft} alt="" />
+        </div>
+        <div className="header-title">{title}</div>
+      </div>
+    );
+  }
+}
--- app/views/trade/trade.component.js
+++ app/views/trade/trade.component.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Find, IsEmpty } from 'react-lodash';
-import Button from '@material-ui/core/Button';
 import './styles.css';
-import Transaction from '../../components/transaction/transaction';
+// import Transaction from '../../components/transaction/transaction';
 import { QR_CODE_PAGE, TRANSFER_PAGE } from '../../constants/navigation';
+import HeaderBack from '../../components/header-back';
+import ButtonCustom from '../../components/common/buttons/button-custom';
 
 export default class Trade extends Component {
   handleBack = () => {
@@ -25,22 +26,74 @@ export default class Trade extends Component {
   };
 
   render() {
-    const { selectedToken, balance, transactions } = this.props;
+    const { selectedToken, balance } = this.props;
+    // const { selectedToken, balance, transactions } = this.props;
     return (
       <React.Fragment>
-        <div>{selectedToken}</div>
+        <HeaderBack handleBack={this.handleBack} title={selectedToken} />
         <IsEmpty
           value={balance.tokens}
           yes={() => <React.Fragment />}
           no={() => (
             <React.Fragment>
-              <Find collection={balance.tokens} predicate={token => token.token === selectedToken}>
-                {token => token.balanceFormatted}
-              </Find>
-              <Transaction className="transaction-container" transactions={transactions} />
-              <Button>SWAP</Button>
-              <Button onClick={this.handleDeposit}>RECEIVE</Button>
-              <Button onClick={this.handleSend}>SEND</Button>
+              <div className="trade-amount-wrapper">
+                <div className="trade-amount">
+                  <Find
+                    collection={balance.tokens}
+                    predicate={token => token.token === selectedToken}
+                  >
+                    {token => token.amount}
+                  </Find>
+                </div>
+                <span className="trade-conversion-value">≈$0</span>
+              </div>
+              <ul className="locked-available-retain-wrapper">
+                <li>
+                  <p>Locked</p>
+                  <span>0</span>
+                </li>
+                <li>
+                  <p>Available</p>
+                  <span>
+                    <Find
+                      collection={balance.tokens}
+                      predicate={token => token.token === selectedToken}
+                    >
+                      {token => token.amount}
+                    </Find>
+                  </span>
+                </li>
+                <li>
+                  <p>Retain</p>
+                  <span>0</span>
+                </li>
+              </ul>
+              {/*<Transaction className="transaction-container" transactions={transactions} />*/}
+              <div
+                style={{
+                  position: 'absolute',
+                  bottom: '11px',
+                  right: '20px',
+                  left: '20px',
+                  justifyContent: 'space-between',
+                  display: 'flex',
+                }}
+              >
+                <ButtonCustom
+                  width="100px"
+                  color="#41485D"
+                  background="white"
+                  border="1px solid rgba(65, 72, 93, 0.5);"
+                >
+                  SWAP
+                </ButtonCustom>
+                <ButtonCustom onClick={this.handleDeposit} width="100px">
+                  RECEIVE
+                </ButtonCustom>
+                <ButtonCustom onClick={this.handleSend} width="100px">
+                  SEND
+                </ButtonCustom>
+              </div>
             </React.Fragment>
           )}
         />
