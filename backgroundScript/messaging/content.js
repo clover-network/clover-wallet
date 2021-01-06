efa486c8494837eaa8f8d798842782a9afc9b486
Deyang Liu <ldy0909061@163.com>
2021-01-06 10:48:38 +0800

    support contract deploy

--- backgroundScript/messaging/content.js
+++ backgroundScript/messaging/content.js
@@ -3,6 +3,7 @@ import * as ResponseService from '../services/response-service';
 import * as MessageTypes from '../../lib/constants/message-types';
 import * as ExtensionApi from '../../lib/services/extension/extension';
 import * as RuntimeApi from '../../lib/services/extension/runtime';
+import * as Web3ResponseService from '../services/web3-response-service';
 
 extension.runtime.onMessage.addListener((request, sender, sendResponse) => {
   // Handle Messages from content
@@ -12,6 +13,7 @@ extension.runtime.onMessage.addListener((request, sender, sendResponse) => {
   const { id: senderId } = sender;
   const extensionId = RuntimeApi.getID();
 
+  // console.log('bg content:', request);
   if (senderId === extensionId) {
     if (senderURL !== popupURL && senderURL !== windowURL) {
       try {
@@ -28,10 +30,14 @@ extension.runtime.onMessage.addListener((request, sender, sendResponse) => {
           case MessageTypes.BG_DAPP_SIGN_MESSAGE:
             ResponseService.signMessage(request, sender, sendResponse);
             break;
+          case MessageTypes.WEB3_REQUEST:
+            Web3ResponseService.web3Response(request, sendResponse);
+            break;
           default:
             ResponseService.handledAppDefault(request, sendResponse);
         }
       } catch (err) {
+        // console.log('bg content err:', err);
         ResponseService.handleProcessingError(request, sendResponse);
       }
     }
--- contentScript/in-page-script.js
+++ contentScript/in-page-script.js
@@ -1,4 +1,5 @@
 import { injectExtension } from '@polkadot/extension-inject';
+import { EventEmitter } from 'events';
 import { resolveRequest } from './messaging/in-page';
 import * as RequestTypes from '../lib/constants/request-types';
 import AppConfig from '../lib/constants/config';
@@ -59,4 +60,69 @@ const enable = async origin => {
 injectExtension(enable, {
   name: AppConfig.name,
   version: AppConfig.version,
+});
+class CloverWalletProvider extends EventEmitter {
+  constructor() {
+    super();
+    this.request = this.request.bind(this);
+    this.send = this.send.bind(this);
+    this.selectedAddress = '0xe6206c7f064c7d77c6d8e3ed8601c9aa435419ce';
+    this.networkVersion = '1337';
+    this.chainId = '0x539';
+    this.setMaxListeners(100);
+  }
+
+  async request(args) {
+    // console.log('request args:', args);
+    const result = await resolveRequest(RequestTypes.WEB3_REQUEST, args, metadata);
+    return result;
+  }
+
+  async send(methodOrPayload, callbackOrArgs) {
+    if (typeof methodOrPayload === 'object' && typeof callbackOrArgs === 'function') {
+      try {
+        const result = await resolveRequest(RequestTypes.WEB3_REQUEST, methodOrPayload, metadata);
+        callbackOrArgs(undefined, result);
+      } catch (e) {
+        callbackOrArgs(e, undefined);
+      }
+    } else {
+      return this._sendSync(methodOrPayload);
+    }
+  }
+
+  _sendSync(payload) {
+    let result;
+    switch (payload.method) {
+      case 'eth_accounts':
+        result = this.selectedAddress ? [this.selectedAddress] : [];
+        break;
+
+      case 'eth_coinbase':
+        result = this.selectedAddress || null;
+        break;
+
+      case 'eth_uninstallFilter':
+        resolveRequest(RequestTypes.WEB3_REQUEST, payload, metadata);
+        result = true;
+        break;
+
+      case 'net_version':
+        result = this.networkVersion || null;
+        break;
+
+      default:
+        throw new Error('unsupported method');
+    }
+
+    return {
+      id: payload.id,
+      jsonrpc: payload.jsonrpc,
+      result,
+    };
+  }
+}
+window.ethereum = new Proxy(new CloverWalletProvider(), {
+  // some common libraries, e.g. web3@1.x, mess with our API
+  deleteProperty: () => true,
 });
