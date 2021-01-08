158479033700ae15f72eb1ca39c6924ac54deee2
Deyang Liu <ldy0909061@gmail.com>
2021-01-08 10:10:41 +0800

    implement a default method to proccess most json/rpc request

--- backgroundScript/services/web3-response-service.js
+++ backgroundScript/services/web3-response-service.js
@@ -1,7 +1,10 @@
 import Web3 from 'web3';
 import * as status from '../../lib/constants/api';
+import * as MessageTypes from '../../lib/constants/message-types';
+import axios from 'axios';
 
-const web3 = new Web3('http://localhost:9933');
+const baseUrl = 'http://localhost:9933'
+const web3 = new Web3(baseUrl);
 // use below messages if no return message is needed
 export const success = {
   status: status.SUCCESS,
@@ -24,11 +27,20 @@ export const requestId = {
   net_version: 67,
   eth_getBalance: 1,
 };
+
 export const handleDefault = async (request, sendResponse) => {
-  const response = {
-    ...failure,
-    message: 'Invalid request.Check message type',
-  };
+  let response;
+  try {
+    response = await axios.post(baseUrl, request.opts)
+    response = { ...success, method: request.opts.method, result: response.data }
+  } catch (e) {
+    response = {
+      ...failure,
+      message: `${request.opts.method} Failed`,
+    };
+  }
+
+  // console.log(`default web3 ${request.opts.method} response result:`, response);
   sendResponse(response);
 };
 
@@ -49,78 +61,48 @@ const buildResult = (id, response) => ({
 export const web3Response = async (request, sendResponse) => {
   let result;
   let transHash;
-  try {
-    switch (request.opts.method) {
-      case 'eth_getBalance':
-        result = await web3.eth.getBalance(request.opts.params[0]);
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'eth_accounts':
-        result = ['0xe6206C7f064c7d77C6d8e3eD8601c9AA435419cE'];
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'net_version':
-        result = await web3.eth.net.getId();
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'eth_getBlockByNumber':
-        result = await web3.eth.getBlock(request.opts.params[0], request.opts.params[1]);
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'eth_estimateGas':
-        result = await web3.eth.estimateGas(request.opts.params[0]);
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'eth_gasPrice':
-        result = await web3.eth.getGasPrice();
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'eth_sendTransaction':
-        const signedTransaction = await web3.eth.accounts.signTransaction(
-          request.opts.params[0],
-          '0xa504b64992e478a6846670237a68ad89b6e42e90de0490273e28e74f084c03c8',
-        );
-        result = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'eth_getTransactionReceipt':
-        transHash = typeof request.opts.params[0] === 'string'
-          ? request.opts.params[0]
-          : request.opts.params[0].transactionHash;
-        result = await web3.eth.getTransactionReceipt(transHash);
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'eth_getTransactionByHash':
-        transHash = typeof request.opts.params[0] === 'string'
-          ? request.opts.params[0]
-          : request.opts.params[0].transactionHash;
-        result = await web3.eth.getTransaction(transHash);
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'eth_getCode':
-        result = await web3.eth.getCode(request.opts.params[0], request.opts.params[1]);
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      case 'eth_call':
-        result = await web3.eth.call(request.opts.params[0], request.opts.params[1]);
-        result = buildResult(request.opts.id, result);
-        sendResponse({ ...success, method: request.opts.method, result });
-        break;
-      default:
-        handleProcessingError(request, sendResponse);
+  if (MessageTypes.SAFE_METHODS.includes(request.opts.method) || request.opts.method === 'eth_accounts') {
+    try {
+      switch (request.opts.method) {
+        case 'eth_accounts':
+          result = ['0xe6206C7f064c7d77C6d8e3eD8601c9AA435419cE'];
+          result = buildResult(request.opts.id, result);
+          sendResponse({ ...success, method: request.opts.method, result });
+          break;
+        case 'eth_sendTransaction':
+          const signedTransaction = await web3.eth.accounts.signTransaction(
+            request.opts.params[0],
+            '0xa504b64992e478a6846670237a68ad89b6e42e90de0490273e28e74f084c03c8',
+          );
+          result = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+          result = buildResult(request.opts.id, result);
+          sendResponse({ ...success, method: request.opts.method, result });
+          break;
+        case 'eth_getTransactionReceipt':
+          transHash = typeof request.opts.params[0] === 'string'
+            ? request.opts.params[0]
+            : request.opts.params[0].transactionHash;
+          result = await web3.eth.getTransactionReceipt(transHash);
+          result = buildResult(request.opts.id, result);
+          sendResponse({ ...success, method: request.opts.method, result });
+          break;
+        case 'eth_getTransactionByHash':
+          transHash = typeof request.opts.params[0] === 'string'
+            ? request.opts.params[0]
+            : request.opts.params[0].transactionHash;
+          result = await web3.eth.getTransaction(transHash);
+          result = buildResult(request.opts.id, result);
+          sendResponse({ ...success, method: request.opts.method, result });
+          break;
+        default:
+          handleDefault(request, sendResponse);
+      }
+      // console.log(`web3 ${request.opts.method} response result:`, result);
+    } catch (e) {
+      sendResponse({ ...failure, message: 'Error in web3Response.' });
     }
-    // console.log(`web3 ${request.opts.method} response result:`, result);
-  } catch (e) {
-    sendResponse({ ...failure, message: 'Error in web3Response.' });
+  } else {
+    handleProcessingError(request, sendResponse);
   }
+  
 };
